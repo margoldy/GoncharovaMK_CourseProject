30379d2154f8678fa719aa2491cb26adbde21d1e
Margarita G. <bestpoc@mail.ru>
2026-05-20 17:50:08 +0300

    backend and frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Семейный бюджет</h1>
+      <p>Твой личный финансовый помощник</p>
+    </div>
+  )
+}
+
+export default App
